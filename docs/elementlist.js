647befdf1ae40ac4b3bfb9bc1229ccebd2139df2
Jacob Chapa <jake@otleylogic.com>
2016-09-16 00:29:20 -0500

    added docs

---
+++ docs/elementlist.js
@@ -0,0 +1,3 @@
+
+var ApiGen = ApiGen || {};
+ApiGen.elements = [["c","codeable_datepicker_widget\\app\\admin\\WidgetAdmin"],["c","codeable_datepicker_widget\\app\\frontend\\DatepickerWidget"],["c","codeable_datepicker_widget\\app\\frontend\\JS_Widget"],["c","Composer\\Autoload\\ClassLoader"],["c","Composer\\Autoload\\ComposerStaticInitf10cddec6f4fbf644d9f1809b4463051"],["c","ComposerAutoloaderInitf10cddec6f4fbf644d9f1809b4463051"],["f","includeFile()"]];
